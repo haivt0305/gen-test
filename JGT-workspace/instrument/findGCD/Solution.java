737afd4e9db044002fbcc81e554b5793b7f5b6c4
Teag John <teagjohnbp@gmail.com>
2022-06-29 14:28:04 +0700

    excel report

---
+++ JGT-workspace/instrument/findGCD/Solution.java
@@ -0,0 +1,96 @@
+import java.io.*;
+public class Solution {
+    private int numerator = 0;
+    private int denominator = 1;
+
+    public Solution(int numerator, int denominator) {
+        if (denominator != 0) {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+    }
+
+
+    public static int findGCD(int x, int y) {
+        try{
+        //Specify the file name and path here
+        File file =new File("/home/tj/Giang/gen-test/JGT-workspace/instrument/findGCD/Solution.java-1978513254.testpath");
+ 
+        /* This logic is to create the file if the
+         * file is not already present
+         */
+        if(!file.exists()){
+           file.createNewFile();
+        }
+ 
+        //Here true is to append the content to file
+        FileWriter fw = new FileWriter(file,true);
+        //BufferedWriter writer give better performance
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write("331CFGIfStatementNode{StartAt:331,EndAt:373" + "\n");
+        //Closing BufferedWriter Stream
+        bw.close();
+ 
+    System.out.println("Data successfully appended at the end of file");
+ 
+      }catch(IOException ioe){
+         System.out.println("Exception occurred:");
+         ioe.printStackTrace();
+       }
+        if(y== 0){
+            try{
+        //Specify the file name and path here
+        File file =new File("/home/tj/Giang/gen-test/JGT-workspace/instrument/findGCD/Solution.java-1978513254.testpath");
+ 
+        /* This logic is to create the file if the
+         * file is not already present
+         */
+        if(!file.exists()){
+           file.createNewFile();
+        }
+ 
+        //Here true is to append the content to file
+        FileWriter fw = new FileWriter(file,true);
+        //BufferedWriter writer give better performance
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write("354CFGReturnStatement{StartAt:354,EndAt:363" + "\n");
+        //Closing BufferedWriter Stream
+        bw.close();
+ 
+    System.out.println("Data successfully appended at the end of file");
+ 
+      }catch(IOException ioe){
+         System.out.println("Exception occurred:");
+         ioe.printStackTrace();
+       }
+            return x;
+        }
+        try{
+        //Specify the file name and path here
+        File file =new File("/home/tj/Giang/gen-test/JGT-workspace/instrument/findGCD/Solution.java-1978513254.testpath");
+ 
+        /* This logic is to create the file if the
+         * file is not already present
+         */
+        if(!file.exists()){
+           file.createNewFile();
+        }
+ 
+        //Here true is to append the content to file
+        FileWriter fw = new FileWriter(file,true);
+        //BufferedWriter writer give better performance
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write("382CFGReturnStatement{StartAt:382,EndAt:405" + "\n");
+        //Closing BufferedWriter Stream
+        bw.close();
+ 
+    System.out.println("Data successfully appended at the end of file");
+ 
+      }catch(IOException ioe){
+         System.out.println("Exception occurred:");
+         ioe.printStackTrace();
+       }
+        return findGCD(y, x%y);
+    }
+    public static void main(String[] args) {Solution.findGCD(-358271056,-1753918035);}
+}
